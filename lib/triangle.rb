174bccd7a27177bcf2a5ccb838648b8a3342ee25
Olga Nadongo <winnienadongo@gmail.com>
2022-08-25 03:35:42 +0300

    complete lab

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
+
 class Triangle
   # write code here
+attr_accessor :base, :height, :hypotenuse
+
+  def initialize(base, height, hypotenuse)
+      @base=base
+      @height=height
+      @hypotenuse=hypotenuse
+  end
+
+  def kind
+    
+  if @base > 0 && @height > 0 &&  @hypotenuse > 0 && @base+@height >  @hypotenuse && @height +  @hypotenuse > @base && @base +  @hypotenuse > @height
+    if self.base == self.height && self.height == self.hypotenuse
+      triangle = :equilateral
+    elsif self.base == self.height || self.height == self.hypotenuse || self.hypotenuse == self.base
+      triangle = :isosceles
+    else
+      triangle = :scalene
+    end
+  else 
+    raise TriangleError
+  end
+  triangle
+ end
+  
+
+
+class TriangleError < StandardError
+  def message
+    puts "This is an invalid triangle because the sides are invalid"
+ end
+end
+
 end
